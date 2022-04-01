7b23454672ddc5eb7a73e5175831ff98cf8581f8
kasper-pawlowski <kaspi28000@gmail.com>
2022-04-01 23:33:10 +0200

    transition fix

--- src/components/atoms/PokemonHeroBg/PokemonHeroBg.styles.js
+++ src/components/atoms/PokemonHeroBg/PokemonHeroBg.styles.js
@@ -2,4 +2,9 @@ import styled from 'styled-components';
 
 export const Svg = styled.svg`
     position: absolute;
+    transition-duration: 1s;
+
+    stop {
+        transition-duration: 0.15s;
+    }
 `;
